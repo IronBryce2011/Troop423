a88a6e844d65f32a17cb9a6618be44cad8a50520
IronBryce2011 <brycetboyd113@gmail.com>
2025-08-04 20:14:47 -0400

    Update Announcements.jsx

--- src/Announcements.jsx
+++ src/Announcements.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-
+axios.defaults.withCredentials = true;
 import './announcement.css';
 
 function AnnouncementDisplay() {
