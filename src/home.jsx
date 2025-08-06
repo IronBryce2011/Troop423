d6bb6f3e75a1c4cb485cee1263fe7d2a02282973
IronBryce2011 <brycetboyd113@gmail.com>
2025-08-06 09:56:51 -0400

    Update home.jsx

--- src/home.jsx
+++ src/home.jsx
@@ -11,7 +11,7 @@ function Home() {
     <div className='home'>
     <div>
       <h1>TROOP 423</h1>
-      <img src="https://i.ibb.co/6pbxLMB/1.jpg" alt="1" border="0" style={{ width: '100%', height: 'auto', display: 'block'}}/>
+      <img src="https://i.ibb.co/xK0L57WB/IMG-4977-1.jpg" alt="1" border="0" style={{ width: '100%', height: 'auto', display: 'block'}}/>
          <h2> Scouting Done Right in Beaver County</h2>
          <p>Troop 423 of the Boy Scouts of America, based in Center Township, Pennsylvania, has proudly served youth in Center Township, and across Beaver County for over 60 years  . Chartered to provide hands‑on outdoor adventures, leadership training, and meaningful community service, Troop 423 unites Scouts and adult volunteers in a tradition of character, camaraderie, and community connection. From camping excursions to supporting local initiatives and annual participation in events, Troop 423 continues to inspire generations of young leaders guided by Scouting values.</p>
           <h3>Links</h3>
