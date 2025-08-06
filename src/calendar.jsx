9ead57fe9448dc5023368241ea0657ce3ea4a0fc
IronBryce2011 <brycetboyd113@gmail.com>
2025-08-06 09:39:11 -0400

    Update calendar.jsx

--- src/calendar.jsx
+++ src/calendar.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 const CalendarEmbed = () => {
   return (
     <iframe
-      src="https://calendar.google.com/calendar/embed?src=1d66f7e90195fa90783a8727ebe02d9b9f6a5b3e332a0a3da86f1c43820ccb52%40group.calendar.google.com&ctz=America%2FNew_York"
+      src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&showPrint=0&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=a2Rma3VndmRwb2F1bmNnbnNnMjd0MWUzN2E5OWF1N2lAaW1wb3J0LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%230b8043&color=%233f51b5"
       style={{ border: 0 }}
       width="800"
       height="600"
