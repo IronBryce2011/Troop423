2c0ee650f4789a4e62e5187cf26c4532d0d0be5d
IronBryce2011 <brycetboyd113@gmail.com>
2025-08-11 14:29:39 -0400

    Update home.jsx

--- src/home.jsx
+++ src/home.jsx
@@ -15,11 +15,12 @@ function Home() {
          <h2> Scouting Done Right in Beaver County</h2>
          <p>Troop 423 of the Boy Scouts of America, based in Center Township, Pennsylvania, has proudly served youth in Center Township, and across Beaver County for over 60 years  . Chartered to provide hands‑on outdoor adventures, leadership training, and meaningful community service, Troop 423 unites Scouts and adult volunteers in a tradition of character, camaraderie, and community connection. From camping excursions to supporting local initiatives and annual participation in events, Troop 423 continues to inspire generations of young leaders guided by Scouting values.</p>
           <h3>Links</h3>
+        <AddLink name='Join the troop!' href='https://beascout.scouting.org/list/?zip=15061&program%5B0%5D=scoutsBSA&scoutsBSAFilter=all&miles=10&unitID=253836' />
          <AddLink name='Laural Highlands Council' href='https://lhcscouting.org/a.org/' />
          <AddLink name='Scoutbook' href='https://scoutbook.scouting.org/?Redir=%2Fmobile%2Fdashboard%2FDefault%2Easp%3F' />
          <AddLink name='BSA Official Site' href='https://www.scouting.org/' />
     
-         <AddLink name='Join the troop!' href='https://beascout.scouting.org/list/?zip=15061&program%5B0%5D=scoutsBSA&scoutsBSAFilter=all&miles=10&unitID=253836' />
+       
     </div>
 
     </div>
